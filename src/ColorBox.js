c040499327f0c2213fdbed645521539d566cec0a
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-07-01 10:32:20 -0300

    style color box

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./ColorBox.css";
 
 export default function ColorBox(props) {
+  const { name, color } = props.color;
   return (
-    <div style={{ background: props.color.color }} className="ColorBox">
-      <span>{props.color.name}</span>
-      <span>MORE</span>
+    <div style={{ background: color }} className="ColorBox">
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">MORE</span>
     </div>
   );
 }
