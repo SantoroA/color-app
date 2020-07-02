7532edcdf04ecf3a098e1cf33218524c3837a6a1
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-07-02 11:25:52 -0300

    styled color slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
+import "./Palette.css";
 
 export default function Palette(props) {
   const { colors } = props.palette;
@@ -12,13 +12,15 @@ export default function Palette(props) {
   }
   return (
     <div className="Palette">
-      <Slider
-        defaultValue={level}
-        min={100}
-        max={900}
-        step={100}
-        onAfterChange={changeLevel}
-      />
+      <div className="slider">
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={changeLevel}
+        />
+      </div>
       <div className="Palette-colors">
         {colors[level].map((color) => (
           <ColorBox color={color} />
