ff1b83d06059e99dfd4b0ed7c8f33ce38f1c4c41
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-07-05 17:48:12 -0300

    added MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,14 @@
 import React from "react";
+import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
 
 export default function PaletteList(props) {
   const { palettes } = props;
   return (
     <div>
+      <MiniPalette />
       {palettes.map((palette) => {
-        return (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
-        );
+        return <MiniPalette {...palette} />;
       })}
     </div>
   );
